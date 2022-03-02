00ff247311802811a363a4d0af3ea2cf9fb5a943
Tommi Riiheläinen <tommi.riihelainen@hotmail.com>
2022-03-02 22:01:05 +0200

    Finished myGrep project

--- main.cpp
+++ main.cpp
@@ -1,201 +1,227 @@
+#include <iostream>
 #include <fstream>
 #include <iomanip>
-#include <iostream>
 #include <string>
 #include <algorithm>
 
 using namespace std;
 
+bool searchFromFile(const string &, string, const string &);
+
+void printLine(const bool &, const int &, const string &);
+
+bool checkCmdLineArgs(const string &, bool &, bool &, bool &, bool &);
+
 void searchFromString();
 
-void printOptions();
+void printUsage();
 
-bool checkCmdLineArgs(string, bool &, bool &, bool &, bool &);
+bool openFile(ifstream &, const string &);
+
 
 int main(int argc, char *argv[])
 {
     cout << "\n";
-    bool getLineNumber{}, getOccurrence{}, getReverse{}, ignoreCase{};
-    int counter{}, found{};
-    string search, fileName, line, tempLine, tempSearch, arg;
-    ifstream file;
 
-    if (argc < 2)
+    switch (argc)
     {
-        searchFromString();
-        return EXIT_SUCCESS;
-    }
-    else if (argc == 3)
-    {
-        search = argv[1];
-        fileName = argv[2];
-    }
-    else
-    {
-        arg = string(argv[1]);
-        search = argv[2];
-        fileName = argv[3];
-    }
-
-
-    file.open(fileName);
-
-    if (file.good())
-    {
-        if (checkCmdLineArgs(arg, getLineNumber, getOccurrence, getReverse, ignoreCase))
-        {
-            while (getline(file, line))
+        case 1:
+            searchFromString();
+            break;
+        case 3:
+            if (!searchFromFile("", argv[1], argv[2]))
             {
-                counter++;
-
-                if (ignoreCase)
-                {
-                    tempSearch = search;
-                    tempLine = line;
-                    transform(search.begin(), search.end(), search.begin(), ::tolower);
-                    transform(line.begin(), line.end(), line.begin(), ::tolower);
-                }
-
-                if (getReverse)
-                {
-                    if (line.find(search) == string::npos)
-                    {
-                        if (getLineNumber)
-                        {
-                            cout << counter << ": ";
-                        }
-                        cout << line << endl;
-                        found++;
-                    }
-                }
-
-                else if (line.find(search) != string::npos)
-                {
-                    if (getLineNumber)
-                    {
-                        cout << counter << ": ";
-                    }
-                    cout << line << endl;
-                    found++;
-                }
+                printUsage();
+                return EXIT_FAILURE;
             }
-
-            if (getOccurrence)
+            break;
+        case 4:
+            if (!searchFromFile(argv[1], argv[2], argv[3]))
             {
-                cout << "\nOccurrences of lines ";
-                if (getReverse)
-                {
-                    cout << "NOT ";
-                }
-                cout << "containing " << quoted(search) << ": " << found << endl;
+                printUsage();
+                return EXIT_FAILURE;
             }
-
-            if (!found)
-            {
-                cout << quoted(search) << " is not included in file." << endl;
-            }
-        }
-
-        else
-        {
-            printOptions();
+            break;
+        default:
+            printUsage();
             return EXIT_FAILURE;
-        }
     }
-
-    else
-    {
-        cerr << "ERROR: Couldn't open file" << endl;
-        return EXIT_FAILURE;
-    }
-
-    file.close();
 
     cout << endl;
     return EXIT_SUCCESS;
 }
 
-void searchFromString()
+
+bool searchFromFile(const string &arg, string search, const string &fileName)
 {
-    string mainString{}, searchString{};
+    bool getLineNumber, getOccurrence, getReverse, ignoreCase;
+    int counter{}, found{};
+    string line, tempLine, tempSearch;
+    ifstream file;
 
-    cout << "Give a string from which to search for: ";
-    getline(cin, mainString);
-    cout << "Give search string: ";
-    getline(cin, searchString);
-
-    size_t found = mainString.find(searchString);
-
-    if (found != string::npos)
+    if (!openFile(file, fileName))
     {
-        cout << quoted(searchString) << " found in " << quoted(mainString)
-             << " at position " << found << "." << endl;
+        return false;
     }
-    else
+
+    if (!arg.empty())
     {
-        cout << quoted(searchString) << " NOT found in " << quoted(mainString)
-             << "." << endl;
+        if (!checkCmdLineArgs(arg, getLineNumber, getOccurrence, getReverse, ignoreCase))
+        {
+            file.close();
+            return false;
+        }
     }
+
+    while (getline(file, line))
+    {
+        counter++;
+
+        tempLine = line;
+        tempSearch = search;
+
+        if (ignoreCase)
+        {
+            transform(line.begin(), line.end(), line.begin(), ::tolower);
+            transform(search.begin(), search.end(), search.begin(), ::tolower);
+        }
+
+        if (getReverse)
+        {
+            if (line.find(search) == string::npos)
+            {
+                printLine(getLineNumber, counter, tempLine);
+                found++;
+            }
+        }
+        else if (line.find(search) != string::npos)
+        {
+            printLine(getLineNumber, counter, tempLine);
+            found++;
+        }
+
+        search = tempSearch;
+    }
+
+
+    if (!found)
+    {
+        cout << quoted(search) << " is not included in file." << endl;
+    }
+    else if (getOccurrence)
+    {
+        cout << "\nOccurrences of lines";
+        cout << (getReverse ? " NOT" : "") << " containing " << quoted(search) << ": " << found << endl;
+    }
+
+
+    file.close();
+
+    return true;
 }
 
-bool checkCmdLineArgs(string arg, bool &getLineNumber, bool &getOccurrence, bool &getReverse, bool &ignoreCase)
+
+void printLine(const bool &getLineNumber, const int &counter, const string &line)
+{
+    if (getLineNumber)
+    {
+        cout << counter << ": ";
+    }
+    cout << line << endl;
+}
+
+
+bool checkCmdLineArgs(const string &arg, bool &getLineNumber, bool &getOccurrence, bool &getReverse, bool &ignoreCase)
 {
     bool lFlag{}, oFlag{}, rFlag{}, iFlag{};
 
     if (arg.length() > 2 && arg[0] == '-' && arg[1] == 'o')
     {
-        try
+        for (int i = 2; i < arg.length(); i++)
         {
-            for (int i = 2; i < arg.length(); i++)
+            switch (arg[i])
             {
-                switch (arg[i])
-                {
-                    case 'l':
-                        if (lFlag)
-                        {
-                            throw;
-                        }
-                        lFlag = true;
-                        getLineNumber = true;
-                        break;
-                    case 'o':
-                        if (oFlag)
-                        {
-                            throw;
-                        }
-                        oFlag = true;
-                        getOccurrence = true;
-                        break;
-                    case 'r':
-                        if (rFlag)
-                        {
-                            throw;
-                        }
-                        rFlag = true;
-                        getReverse = true;
-                        break;
-                    case 'i':
-                        if (iFlag)
-                        {
-                            throw 6;
-                        }
-                        iFlag = true;
-                        ignoreCase = true;
-                        break;
-                }
+                case 'l':
+                    if (lFlag)
+                    {
+                        return false;
+                    }
+                    lFlag = true;
+                    getLineNumber = true;
+                    break;
+                case 'o':
+                    if (oFlag)
+                    {
+                        return false;
+                    }
+                    oFlag = true;
+                    getOccurrence = true;
+                    break;
+                case 'r':
+                    if (rFlag)
+                    {
+                        return false;
+                    }
+                    rFlag = true;
+                    getReverse = true;
+                    break;
+                case 'i':
+                    if (iFlag)
+                    {
+                        return false;
+                    }
+                    iFlag = true;
+                    ignoreCase = true;
+                    break;
             }
         }
-
-
         return true;
     }
 
     return false;
 }
 
-void printOptions()
+
+void searchFromString()
 {
+    string userLine{}, search{};
+
+    cout << "Give a string from which to search for: ";
+    getline(cin, userLine);
+    cout << "Give search string: ";
+    getline(cin, search);
+
+    size_t found = userLine.find(search);
+
+    if (found == string::npos)
+    {
+        cout << quoted(search) << " NOT found in " << quoted(userLine)
+             << "." << endl;
+        return;
+    }
+
+    cout << quoted(search) << " found in " << quoted(userLine)
+         << " at position " << found << "." << endl;
+}
+
+
+bool openFile(ifstream &file, const string &fileName)
+{
+    file.open(fileName);
+
+    if (!file.good())
+    {
+        cerr << "ERROR: Couldn't open file\n" << endl;
+        return false;
+    }
+
+    return true;
+}
+
+
+void printUsage()
+{
+    cout << "Usage: ./myGrep [[options] [keyword fileName]]\n\n";
     cout << "Available options (access with -o)\n l - Print line number\n o - Print occurrences\n r - Reverse search\n i - Ignore "
             "case\n\n";
 }
-
